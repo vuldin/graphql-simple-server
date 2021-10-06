afde23cc633c73bd9c9041979b4b8ed39dad940d
Josh Purcell <josh.purcell@ibm.com>
2021-10-06 15:25:20 -0400

    add logging, dependency updates

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server')
+
+const myPlugin = {
+  // Fires whenever a GraphQL request is received from a client.
+  async requestDidStart(requestContext) {
+    console.log('Request started! Query:\n' + requestContext.request.query)
+
+    return {
+      // Fires whenever Apollo Server will parse a GraphQL
+      // request to create its associated document AST.
+      async parsingDidStart(requestContext) {
+        console.log('Parsing started!')
+      },
+
+      // Fires whenever Apollo Server will validate a
+      // request's document AST against your GraphQL schema.
+      async validationDidStart(requestContext) {
+        console.log('Validation started!')
+      },
+    }
+  },
+}
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
@@ -12,7 +33,7 @@ const books = [
     title: 'Jurassic Park',
     author: 'Michael Crichton',
   },
-];
+]
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
@@ -30,7 +51,7 @@ const typeDefs = gql`
   type Query {
     books: [Book]
   }
-`;
+`
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
@@ -38,15 +59,19 @@ const resolvers = {
   Query: {
     books: () => books,
   },
-};
+}
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [myPlugin],
+})
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  console.log(`🚀  Server ready at ${url}`)
+})
